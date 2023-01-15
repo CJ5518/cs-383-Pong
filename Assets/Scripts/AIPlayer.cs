7d5db906222a20ab6bfb551b0a66b60cc1155fe4
CarsonRueber <carsonrueber@gmail.com>
2023-01-14 17:48:46 -0800

    Initial commit

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AIPlayer : MonoBehaviour {
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
